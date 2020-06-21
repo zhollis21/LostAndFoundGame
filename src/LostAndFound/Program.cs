6b4e0f830f269122c0b28ec94c9c1d5ae5873752
Zach Hollis <43797367+zhollis21@users.noreply.github.com>
2020-06-20 21:23:52 -0400

    Added a C# solution and project.

---
+++ src/LostAndFound/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LostAndFound
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
