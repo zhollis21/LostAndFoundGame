99b260ad602bdd8b485eb1c832179d597a55cb03
Paula Oliveros Hollis <paulahollis21@gmail.com>
2020-06-22 22:02:56 -0400

    Added a fuction which prints the main menu. Created a function in charge of getting the user's menu choice and validating it. Lastly, I added the ManageGameActions() which will handle all the menu, user input and anything else related to any actions that need to be taken as a result of the main menu choice.

--- src/LostAndFound/GameManager.cs
+++ src/LostAndFound/GameManager.cs
@@ -1,10 +1,14 @@
-﻿namespace LostAndFound
+﻿using System;
+
+namespace LostAndFound
 {
     public static class GameManager
     {
         public static void StartGame()
         {
             PrintIntro();
+
+            ManageGameActions();
         }
 
         private static void PrintIntro()
@@ -15,6 +19,38 @@
                 "\n\n\t\t\tDeveloped by: Zach and Paula Hollis\n\n";
 
             TimedEvents.TimedWrite(introText, TimedEvents.WriteSpeed.Slow);
+        }
+
+        private static void ManageGameActions()
+        {
+            PrintMainMenu();
+            char userInput = GetUserInput();
+        }
+
+        private static void PrintMainMenu()
+        {
+            var menuText =
+                "\n\nMain Menu:\n\t" +
+                "Press 'S' to Start a New Game\n\t" +
+                "Press 'C' to Continue a Previous Game\n\t" +
+                "Press 'L' to View the Leaderboards\n\t" +
+                "Press 'H' for Help and FAQ\n\t" +
+                "Press 'Q' to Quit\n";
+
+            TimedEvents.TimedWrite(menuText, TimedEvents.WriteSpeed.Fast);
+        }
+
+        private static char GetUserInput()
+        {
+            char userInput = char.ToLower(Console.ReadKey(true).KeyChar);
+
+            while(userInput != 's' && userInput != 'c' && userInput != 'l' && userInput != 'h' && userInput != 'q')
+            {
+                Console.Write("\n\nYour input is not valid. Please select one of the Main Menu options.");
+                userInput = char.ToLower(Console.ReadKey(true).KeyChar);
+            }          
+
+            return userInput;
         }
     }
 }
