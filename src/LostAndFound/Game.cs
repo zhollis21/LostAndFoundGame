a55a5fe19aa8712ae56cc4ee896ac7a0e45a1020
Paula Oliveros Hollis <paulahollis21@gmail.com>
2020-06-28 15:48:46 -0400

    Created Game class and stubbed out two of it's methods. This class will handle anything related to playing the game. I also implemented the HelpUser method and hooked up the calls for each of the menu choices in the MenuActions method.

---
+++ src/LostAndFound/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LostAndFound
+{
+    public static class Game
+    {
+        public static void StartNewGame()
+        {
+            Console.WriteLine("\nPlaying the game...");
+        }
+
+        public static void ContinuePreviousGame()
+        {
+            Console.WriteLine("\nContinuing a game...");
+        }
+    }
+}
--- src/LostAndFound/GameManager.cs
+++ src/LostAndFound/GameManager.cs
@@ -4,13 +4,13 @@ namespace LostAndFound
 {
     public static class GameManager
     {
-        private enum MenuChoice { Start, Continue, Leaderboards, Help, Quit}
+        private enum MenuChoice { Start, Continue, Leaderboard, Help, Quit }
 
         public static void StartGame()
         {
             PrintIntro();
 
-            ManageGameActions();
+            ManageMenuActions();
         }
 
         private static void PrintIntro()
@@ -23,10 +23,35 @@ namespace LostAndFound
             TimedEvents.TimedWrite(introText, TimedEvents.WriteSpeed.Slow);
         }
 
-        private static void ManageGameActions()
+        private static void ManageMenuActions()
         {
             PrintMainMenu();
             MenuChoice userMenuChoice = GetUserMenuChoice();
+
+            if (userMenuChoice == MenuChoice.Start)
+            {
+                Game.StartNewGame();
+            }
+
+            if (userMenuChoice == MenuChoice.Continue)
+            {
+                Game.ContinuePreviousGame();
+            }
+
+            if (userMenuChoice == MenuChoice.Leaderboard)
+            {
+                PrintLeaderboard();
+            }
+
+            if (userMenuChoice == MenuChoice.Help)
+            {
+                HelpUser();
+            }
+
+            if (userMenuChoice == MenuChoice.Quit)
+            {
+                TimedEvents.TimedWrite("\n\n\tThank you for playing!", TimedEvents.WriteSpeed.Slow);
+            }
         }
 
         private static void PrintMainMenu()
@@ -35,7 +60,7 @@ namespace LostAndFound
                 "\n\nMain Menu:\n\t" +
                 "Press 'S' to Start a New Game\n\t" +
                 "Press 'C' to Continue a Previous Game\n\t" +
-                "Press 'L' to View the Leaderboards\n\t" +
+                "Press 'L' to View the Leaderboard\n\t" +
                 "Press 'H' for Help and FAQ\n\t" +
                 "Press 'Q' to Quit\n";
 
@@ -43,7 +68,7 @@ namespace LostAndFound
         }
 
         private static MenuChoice GetUserMenuChoice()
-        { 
+        {
             while (true)
             {
                 char userMenuChoice = char.ToUpper(Console.ReadKey(true).KeyChar);
@@ -55,7 +80,7 @@ namespace LostAndFound
                     return MenuChoice.Continue;
 
                 if (userMenuChoice == 'L')
-                    return MenuChoice.Leaderboards;
+                    return MenuChoice.Leaderboard;
 
                 if (userMenuChoice == 'H')
                     return MenuChoice.Help;
@@ -63,6 +88,33 @@ namespace LostAndFound
                 if (userMenuChoice == 'Q')
                     return MenuChoice.Quit;
             }
+        }
+
+        private static void PrintLeaderboard()
+        {
+            Console.WriteLine("\nPrinting the Leaderboad...");
+        }
+
+        private static void HelpUser()
+        {
+            string helpText =
+                "\n\nHelp:\n\t" +                
+                "\n\tLost and Found is all about making smart choices.\n\t" +
+                "The game gives you the freedom to do anything you wish, but those choices might have swift consequences...\n\t" +
+                "\n\tHaving trouble surviving?\n\t" +
+                "Health can be recovered by sleeping or keeping your hunger level moderate to full.\n\t" +
+                "Try to limit your exploring until to have some food stored up.\n\t" +
+                "When exploring, try making a sturdy camp in the center of a region to use as a base of operations.\n\t" +
+                "\n\tDoes your game freeze up when you select \"Continue\"?\n\t" +
+                "Most likely your game data file has been moved or corrupted.\n\t" +
+                "Unfortunately you will need to start a new game.\n\t" +
+                "\n\tEncountering bugs or glitches?\n\t" +
+                "Contact the developers at: zhollis21@gmail.com\n\t" +
+                "Be sure to include a detailed discription of the problem and a screenshot if possible.\n\t" +
+                "\n\tThanks for playing!" +
+                "\n\nPress anything to return to the Main Menu...\n";
+
+            TimedEvents.TimedWrite(helpText, TimedEvents.WriteSpeed.Fast);
         }
     }
 }
