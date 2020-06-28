1268cad9d3cfd53d12f847529980a8ddd5360834
Paula Oliveros Hollis <50373865+paulaOliverosHollis@users.noreply.github.com>
2020-06-27 21:16:27 -0400

    Main menu and user input handling  (#2)
    * Added a fuction which prints the main menu. Created a function in charge of getting the user's menu choice and validating it. Lastly, I added the ManageGameActions() which will handle all the menu, user input and anything else related to any actions that need to be taken as a result of the main menu choice.
    
    * The GetUserInput method now returns an enum instead of a char for ease of use purpose. I also refactored the way invalid menu choices are handled. There  is not an error message anymore, instead we do not do anything until we get a valid input.
    
    * Changed the name of GetUserInput() to GetUserMenuChoice since this name represents more accurately what the method actually does. I also modified the elements of the MenuChoice enum, they all have more descriptive names now. There were also a couple of variable names changes in order to improve clarity of the code.

--- src/LostAndFound/GameManager.cs
+++ src/LostAndFound/GameManager.cs
@@ -1,10 +1,16 @@
-﻿namespace LostAndFound
+﻿using System;
+
+namespace LostAndFound
 {
     public static class GameManager
     {
+        private enum MenuChoice { Start, Continue, Leaderboards, Help, Quit}
+
         public static void StartGame()
         {
             PrintIntro();
+
+            ManageGameActions();
         }
 
         private static void PrintIntro()
@@ -15,6 +21,48 @@
                 "\n\n\t\t\tDeveloped by: Zach and Paula Hollis\n\n";
 
             TimedEvents.TimedWrite(introText, TimedEvents.WriteSpeed.Slow);
+        }
+
+        private static void ManageGameActions()
+        {
+            PrintMainMenu();
+            MenuChoice userMenuChoice = GetUserMenuChoice();
+        }
+
+        private static void PrintMainMenu()
+        {
+            var menuText =
+                "\n\nMain Menu:\n\t" +
+                "Press 'S' to Start a New Game\n\t" +
+                "Press 'C' to Continue a Previous Game\n\t" +
+                "Press 'L' to View the Leaderboards\n\t" +
+                "Press 'H' for Help and FAQ\n\t" +
+                "Press 'Q' to Quit\n";
+
+            TimedEvents.TimedWrite(menuText, TimedEvents.WriteSpeed.Fast);
+        }
+
+        private static MenuChoice GetUserMenuChoice()
+        { 
+            while (true)
+            {
+                char userMenuChoice = char.ToUpper(Console.ReadKey(true).KeyChar);
+
+                if (userMenuChoice == 'S')
+                    return MenuChoice.Start;
+
+                if (userMenuChoice == 'C')
+                    return MenuChoice.Continue;
+
+                if (userMenuChoice == 'L')
+                    return MenuChoice.Leaderboards;
+
+                if (userMenuChoice == 'H')
+                    return MenuChoice.Help;
+
+                if (userMenuChoice == 'Q')
+                    return MenuChoice.Quit;
+            }
         }
     }
 }
